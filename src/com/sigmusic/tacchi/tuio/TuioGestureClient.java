f65102bcab1b740033acf10c09df17e591cb8595
rj <rj@mobile-109-209.near.uiuc.edu>
2011-03-10 03:06:09 -0600

    Useful functions

--- src/com/sigmusic/tacchi/tuio/TuioGestureClient.java
+++ src/com/sigmusic/tacchi/tuio/TuioGestureClient.java
@@ -1,13 +1,18 @@
 package com.sigmusic.tacchi.tuio;
 
+import java.util.ArrayList;
+
 import TUIO.TuioClient;
-import TUIO.TuioContainer;
 import TUIO.TuioCursor;
 import TUIO.TuioListener;
 import TUIO.TuioObject;
+import TUIO.TuioPoint;
 import TUIO.TuioTime;
 
 public class TuioGestureClient implements TuioListener {
+	int MAX_CURSORS = 5;
+	
+	ArrayList<TuioCursor> pastCursors = new ArrayList<TuioCursor>();
 	
 	public TuioGestureClient(TuioClient client) {
 		client.addTuioListener(this);
@@ -15,43 +20,84 @@ public class TuioGestureClient implements TuioListener {
 
 	@Override
 	public void addTuioCursor(TuioCursor tcur) {
-		// TODO Auto-generated method stub
-		System.out.println("cursor added! "+tcur);
+//		System.out.println("cursor added! "+tcur);
 	}
 
 	@Override
 	public void addTuioObject(TuioObject tobj) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void refresh(TuioTime ftime) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void removeTuioCursor(TuioCursor tcur) {
-		// TODO Auto-generated method stub
+		if (pastCursors.size() > MAX_CURSORS) {
+			pastCursors.remove(0);
+		}
+		pastCursors.add(tcur);
+		
+		/**
+		 * check:
+		 * were the last 5 cursors to be removed from the screen started at the same point?
+		 */
+		
+		if (pastCursors.size() == MAX_CURSORS) {
+			float minstartdistance = minCircle(pastCursors, 0);
+			System.out.println("Min start distance: "+minstartdistance);
+			pastCursors.clear();
+		}
+		
+		
+		
 		
 	}
+	
+	
+	float minCircle(ArrayList<TuioCursor> cursors, int index) {
+		float totalMinimum = 100f;
+		for (TuioCursor cursor : cursors) {
+			float max = maxDistance(cursors, index, cursor);
+			if (max < totalMinimum) 
+				totalMinimum = max;
+		}
+		return totalMinimum;
+	}
+	
+	float maxDistance(ArrayList<TuioCursor> cursors, int index, TuioCursor targetCursor) {
+		TuioPoint pivotPoint = targetCursor.getPath().get(index);
+		float maxDistance = 0;
+		for (TuioCursor cursor : cursors) {
+			TuioPoint point = cursor.getPath().get(index);
+			float distance = point.getDistance(pivotPoint);
+			if (distance > maxDistance) {
+				maxDistance = distance;
+			}
+		}
+		
+		return maxDistance;
+	}
+
+	
+	
+	
+	
 
 	@Override
 	public void removeTuioObject(TuioObject tobj) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void updateTuioCursor(TuioCursor tcur) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void updateTuioObject(TuioObject tobj) {
-		// TODO Auto-generated method stub
 		
 	}
 	
